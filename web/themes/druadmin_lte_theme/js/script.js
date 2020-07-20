f96dfbd31299063905b6a14d29e8a5be32305a0d
mnicodev <nicolasmoinot@webstep.fr>
2020-07-20 19:39:41 +0200

    fin de journée 20072020

--- web/themes/druadmin_lte_theme/js/script.js
+++ web/themes/druadmin_lte_theme/js/script.js
@@ -2,6 +2,12 @@ $ = jQuery;
 
 $(function(){
 
+	$.fn.getAjaxCoordonnees = function(argument) {
+		    console.log('myAjaxCallback is called.');
+		    // Set textfield's value to the passed arguments.
+		$("#edit-field-lieu-wrapper").html(argument);
+	};
+
 	$('ol.breadcrumb li').each(function(index, value){
 		if( $.trim($(value).html()).length == 0 )
 			$(value).remove();
