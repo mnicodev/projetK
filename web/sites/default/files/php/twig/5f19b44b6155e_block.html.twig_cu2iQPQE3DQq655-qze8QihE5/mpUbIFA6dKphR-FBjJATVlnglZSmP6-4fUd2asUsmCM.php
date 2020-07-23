6e2315c2c7152a6dc3ac7c11739aaf047c6c3e08
mnicodev <nicolasmoinot@webstep.fr>
2020-07-23 18:02:25 +0200

    copie 23/07/2020

---
+++ web/sites/default/files/php/twig/5f19b44b6155e_block.html.twig_cu2iQPQE3DQq655-qze8QihE5/mpUbIFA6dKphR-FBjJATVlnglZSmP6-4fUd2asUsmCM.php
@@ -0,0 +1,98 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/druadmin_lte_theme/templates/block/block.html.twig */
+class __TwigTemplate_e6d82276f1011b0ed3411a936455ad79da149aab93b05bb54f129b5c4b0d6adc extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+            'content' => [$this, 'block_content'],
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = ["block" => 29];
+        $filters = ["escape" => 29];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['block'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453 = $this->env->getExtension("Drupal\\webprofiler\\Twig\\Extension\\ProfilerExtension");
+        $__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453->enter($__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "themes/druadmin_lte_theme/templates/block/block.html.twig"));
+
+        // line 29
+        $this->displayBlock('content', $context, $blocks);
+        
+        $__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453->leave($__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453_prof);
+
+    }
+
+    public function block_content($context, array $blocks = [])
+    {
+        $__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453 = $this->env->getExtension("Drupal\\webprofiler\\Twig\\Extension\\ProfilerExtension");
+        $__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453->enter($__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "content"));
+
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["content"] ?? null)), "html", null, true);
+        
+        $__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453->leave($__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/druadmin_lte_theme/templates/block/block.html.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  59 => 29,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "themes/druadmin_lte_theme/templates/block/block.html.twig", "/home/nico/projetK/web/themes/druadmin_lte_theme/templates/block/block.html.twig");
+    }
+}
--- web/themes/druadmin_lte_theme/js/script.js
+++ web/themes/druadmin_lte_theme/js/script.js
@@ -1,11 +1,37 @@
 $ = jQuery;
 
 $(function(){
+	
+	$(".node--type-activite").find("#edit-field-date-wrapper").find(".paragraphs-dropbutton-wrapper").remove();
+	
+	$.fn.getAjaxVille = function(argument) {
+		 console.log(argument);
+		 $("#edit-field-ville").html("")
+//$("#edit-field-ville-wrapper").html(argument);
+		 for(item in argument) {
+		 	$("#edit-field-ville").append("<option value='"+argument[item].key+"'>"+argument[item].val+"</option>");
+		 }
+		 
+	}
 
 	$.fn.getAjaxCoordonnees = function(argument) {
-		    console.log('myAjaxCallback is called.');
-		    // Set textfield's value to the passed arguments.
-		$("#edit-field-lieu-wrapper").html(argument);
+		    console.log(argument);
+		$("#edit-field-adresse-wrapper").find("input").val("");
+		$("#edit-field-telephone-wrapper").find("textarea").val("");
+		$("#edit-field-email-wrapper").find("input").val("");
+		$("#edit-field-code-postal-wrapper").find("input").val("");
+		$("#edit-field-lien-wrapper").find("input").val("");
+		if(argument.adresse) $("#edit-field-adresse-wrapper").find("input").val(argument.adresse.value);
+		if(argument.code_postal) $("#edit-field-code-postal-wrapper").find("input").val(argument.code_postal.value);
+		if(argument.telephone) $("#edit-field-telephone-wrapper").find("textarea").val(argument.telephone.value);
+		if(argument.email) $("#edit-field-email-wrapper").find("input").val(argument.email.value);
+		if(argument.lien) $("#edit-field-lien-wrapper").find("input").val(argument.lien.value);
+		if(argument.ville) {
+			$("#edit-field-ville").find("option").each(function () {
+				if($(this).val()!=argument.ville.target_id) $(this).remove();
+				console.log($(this).val());
+			})
+		}
 	};
 
 	$('ol.breadcrumb li').each(function(index, value){
