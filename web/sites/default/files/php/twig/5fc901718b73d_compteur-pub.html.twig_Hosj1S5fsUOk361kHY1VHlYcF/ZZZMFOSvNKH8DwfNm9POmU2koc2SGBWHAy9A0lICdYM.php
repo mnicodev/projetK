10cec3091821dc041697fc43d86100a27e0ff804
mnicodev <nicolasmoinot@webstep.fr>
2020-12-09 11:01:45 +0100

    update 2020 12 09

---
+++ web/sites/default/files/php/twig/5fc901718b73d_compteur-pub.html.twig_Hosj1S5fsUOk361kHY1VHlYcF/ZZZMFOSvNKH8DwfNm9POmU2koc2SGBWHAy9A0lICdYM.php
@@ -0,0 +1,164 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* modules/custom/kidiklik_admin/templates/compteur-pub.html.twig */
+class __TwigTemplate_b44a18dcb2329aaaa0b58c656dc403139854f93a438f1e22c04a4eadd94d5cce extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = ["for" => 4];
+        $filters = ["escape" => 6, "replace" => 6];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['for'],
+                ['escape', 'replace'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453 = $this->env->getExtension("Drupal\\webprofiler\\Twig\\Extension\\ProfilerExtension");
+        $__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453->enter($__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "modules/custom/kidiklik_admin/templates/compteur-pub.html.twig"));
+
+        // line 1
+        echo "compteur pub
+
+<table class=\"views-table views-view-table cols-10 sticky-enabled table table-bordered  responsive\">
+";
+        // line 4
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable(($context["pub"] ?? null));
+        foreach ($context['_seq'] as $context["key"] => $context["line"]) {
+            // line 5
+            echo "<tr>
+<td colspan='4' class=\"alert alert-dark\">du ";
+            // line 6
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, twig_replace_filter($this->sandbox->ensureToStringAllowed($context["key"]), ["_" => " au "]), "html", null, true);
+            echo "</td>
+</tr>
+
+    ";
+            // line 9
+            $context['_parent'] = $context;
+            $context['_seq'] = twig_ensure_traversable($context["line"]);
+            foreach ($context['_seq'] as $context["_key"] => $context["item"]) {
+                // line 10
+                echo "    <tr>
+    <td>";
+                // line 11
+                echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute($context["item"], "title", [])), "html", null, true);
+                echo "</td>
+    <td>";
+                // line 12
+                echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute($context["item"], "format", [])), "html", null, true);
+                echo "</td>
+    <td>";
+                // line 13
+                echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute($context["item"], "date_deb", [])), "html", null, true);
+                echo "</td>
+    <td>";
+                // line 14
+                echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute($context["item"], "date_fin", [])), "html", null, true);
+                echo "</td>
+    </tr>
+    ";
+            }
+            $_parent = $context['_parent'];
+            unset($context['_seq'], $context['_iterated'], $context['_key'], $context['item'], $context['_parent'], $context['loop']);
+            $context = array_intersect_key($context, $_parent) + $_parent;
+            // line 17
+            echo "
+";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['key'], $context['line'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 19
+        echo "</table>
+";
+        
+        $__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453->leave($__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "modules/custom/kidiklik_admin/templates/compteur-pub.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  111 => 19,  104 => 17,  95 => 14,  91 => 13,  87 => 12,  83 => 11,  80 => 10,  76 => 9,  70 => 6,  67 => 5,  63 => 4,  58 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("compteur pub
+
+<table class=\"views-table views-view-table cols-10 sticky-enabled table table-bordered  responsive\">
+{% for key,line in pub %}
+<tr>
+<td colspan='4' class=\"alert alert-dark\">du {{ key|replace({\"_\":\" au \"}) }}</td>
+</tr>
+
+    {% for item in line %}
+    <tr>
+    <td>{{ item.title }}</td>
+    <td>{{ item.format }}</td>
+    <td>{{ item.date_deb }}</td>
+    <td>{{ item.date_fin }}</td>
+    </tr>
+    {% endfor %}
+
+{% endfor %}
+</table>
+", "modules/custom/kidiklik_admin/templates/compteur-pub.html.twig", "/home/nico/projetK/web/modules/custom/kidiklik_admin/templates/compteur-pub.html.twig");
+    }
+}
---
+++ web/sites/default/files/php/twig/5fc901718b73d_compteur-pub.html.twig_Hosj1S5fsUOk361kHY1VHlYcF/ece6Qpx40vHxbXRW35suTQT0K9RRiQjQ-CgejhnxNPQ.php
@@ -0,0 +1,115 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* modules/custom/kidiklik_admin/templates/compteur-pub.html.twig */
+class __TwigTemplate_b44a18dcb2329aaaa0b58c656dc403139854f93a438f1e22c04a4eadd94d5cce extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = ["for" => 3];
+        $filters = ["escape" => 4];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['for'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453 = $this->env->getExtension("Drupal\\webprofiler\\Twig\\Extension\\ProfilerExtension");
+        $__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453->enter($__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "modules/custom/kidiklik_admin/templates/compteur-pub.html.twig"));
+
+        // line 1
+        echo "compteur pub
+
+";
+        // line 3
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable(($context["pub"] ?? null));
+        foreach ($context['_seq'] as $context["key"] => $context["item"]) {
+            // line 4
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($context["key"]), "html", null, true);
+            echo "
+";
+            // line 5
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute($this->getAttribute(($context["pub"] ?? null), $context["key"], [], "array"), "format", [])), "html", null, true);
+            echo "
+";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['key'], $context['item'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        
+        $__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453->leave($__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "modules/custom/kidiklik_admin/templates/compteur-pub.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  70 => 5,  66 => 4,  62 => 3,  58 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("compteur pub
+
+{% for key,item in pub %}
+{{ key }}
+{{ pub[key].format }}
+{% endfor %}
+", "modules/custom/kidiklik_admin/templates/compteur-pub.html.twig", "/home/nico/projetK/web/modules/custom/kidiklik_admin/templates/compteur-pub.html.twig");
+    }
+}
